279837d94f09dba74a1665bb426b566ae7ff5db9
zhangkai <871227769@qq.com>
2019-04-29 10:34:36 +0800

    Update 源.cpp

--- 腾讯2018春招技术类编程题汇总/1/1/源.cpp
+++ 腾讯2018春招技术类编程题汇总/1/1/源.cpp
@@ -1,10 +1,15 @@
+/*小Q定义了一种数列称为翻转数列:
+给定整数n和m, 满足n能被2m整除。对于一串连续递增整数数列1, 2, 3, 4..., 每隔m个符号翻转一次, 最初符号为'-';。
+例如n = 8, m = 2, 数列就是: -1, -2, +3, +4, -5, -6, +7, +8.
+而n = 4, m = 1, 数列就是: -1, +2, -3, + 4.
+小Q现在希望你能帮他算算前n项和为多少。*/
 #include<iostream>
 #include<cstdio>
 using namespace std;
 
 int main()
 {
-	long long int n = 0;//�������long long
+	long long int n = 0;//Êý´ó¾ÍÓÃlong long
 	long long int m = 0;
 	while (cin >> n >> m)
 	{
